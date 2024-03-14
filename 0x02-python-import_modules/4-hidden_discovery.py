3c24ea7820bc5a2cc55b40f05983a758f284a732
Amr995 <amr995@yahoo.com>
2024-03-14 15:38:41 -0700

    4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,13 @@
-#!/usr.bin.python3
-if __name__ == "__main__":
-    """Print all hidden directories"""
-    import hidden_4
+#!/usr/bin/python3
+import hidden_4
 
-    for i in dir(hidden_4):
-        if i[:2] != "__":
-            print(i)
+
+def discovery():
+    name = dir(hidden_4)
+    for i in name:
+        if i[:2] != '__':
+            print("{:s}".format(i))
+
+
+if __name__ == "__main__":
+    discovery()
