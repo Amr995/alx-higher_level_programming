35ee34ce626b04c9aab079890cd4da753974594d
Amr995 <amr995@yahoo.com>
2024-04-07 13:15:28 -0700

    0x05

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        value = int(input("Enter a number: "))
-    except valueError:
-        return False
-    else:
+        print('{:d}'.format(value))
         return True
+    except (TypeError, ValueError):
+        return False
