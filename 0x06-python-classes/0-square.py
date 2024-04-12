1721efb4514041fd4438147d2fa29dcede75e981
Amr995 <amr995@yahoo.com>
2024-04-12 05:15:09 -0700

    R & 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,2 @@
 #!/usr/bin/python3
-"""Square module."""
-
 class Square:
-    """Defines a square."""
-    def __init___(self, square):
-        self.square = Square
-    def get_square(self):
-        return self.square
