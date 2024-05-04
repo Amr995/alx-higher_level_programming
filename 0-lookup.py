a88cb7fd4a623eac8f49f4e22b677fdb031a426c
Amr995 <amr995@yahoo.com>
2024-05-04 18:37:55 +0000

    m

---
+++ 0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+============================
+dule with the method lookpu
+============================
+"""
+
+
+def lookup(obj):
+    """Function for return the attributes for an object"""
+
+    return dir(obj)
