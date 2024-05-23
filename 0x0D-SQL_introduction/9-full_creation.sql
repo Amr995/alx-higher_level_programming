7f00f71b4fc107eee76b7692e9d77d4f336d2ad5
Amr995 <amr995@yahoo.com>
2024-05-23 12:02:20 +0000

    secont table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,9 +1,10 @@
 -- Create table and insert multiple registers
 -- Execute: cat 9-full_creation.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table VALUES (
-(1, "John", 10),
-(2, "Alex", 3),
-(3, "Bob", 14),
-(4, "George", 8)
-);
+INSERT INTO second_table
+	(id, name, score)
+VALUES
+	(1, "John", 10),
+	(2, "Alex", 3),
+	(3, "Bob", 14),
+	(4, "George", 8);
