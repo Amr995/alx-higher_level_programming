e9bd29891176e04b8ff07c8585db489db8d803a9
Amr995 <amr995@yahoo.com>
2024-05-24 15:24:14 +0000

    m

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -5,11 +5,11 @@ ALTER DATABASE
 	hbtn_0c_0
 	CHARACTER SET = utf8mb4
 	COLLATE = utf8mb4_unicode_ci;
-ALTER DATEBASE
+ALTER TABLE
 	first_table
 	CONVERT TO CHARACTER SET utf8mb4
 	COLLATE = utf8mb4_unicode_ci;
-ALTER DATEBASE
+ALTER TABLE
 	first_table
 	CHANGE name VARCHAR(256)
 	CHARACTER SET utf8mb4
