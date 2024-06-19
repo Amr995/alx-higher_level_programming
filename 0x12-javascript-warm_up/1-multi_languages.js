f35259f3235ff7c7b37e6e65477b1b225fb7e280
Amr995 <amr995@yahoo.com>
2024-06-19 05:55:03 -0700

    1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
 // Print 3 lines "C is fun", "Python is cool", "JavaScript is amazing"
 
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
