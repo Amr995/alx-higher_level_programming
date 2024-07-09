5b026ddbc90e26f7983bfd6aa7b12ee8e8bf86f7
Amr995 <amr995@yahoo.com>
2024-07-09 08:50:27 -0700

    0x13

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
-class Rectangle{
-	constructor(w, h) {
-		if (w > 0 && h > 0) {
-		this.width = w;
-		this.height = h;
-		}
-	}
-}
-module.exports = Rectangle;
+
+// script that creates a class.
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};
